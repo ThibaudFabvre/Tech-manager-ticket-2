e5db2fba60d949951c50e246a5fb5609098bd731
Thibaud Fabvre <thibaudfabvre@thibauds-macbook-pro.home>
2023-11-29 23:42:51 +0100

    [INTRO] add mongoDB support

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import express, { Express, Request, Response, Application } from 'express';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 
 //For env File
 dotenv.config();
 
+const client = new MongoClient(process.env.DB_URI as string);
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const database = client.db('tech-manager');
+    const collection = database.collection('tickets');
+
+    const query = { name: 'starting' };
+    const ticket = await collection.findOne(query);
+    console.log(ticket);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
   res.send('Welcome to Express & TypeScript Server');
 });
+app.post('/');
 
 app.listen(port, () => {
   console.log(`Server is operational on http://localhost:${port}`);
