0274dc1d65ee62e076a693f554bd5d4abfd82bf9
Thibaud Fabvre <thibaudfabvre@thibauds-macbook-pro.home>
2023-11-29 23:49:43 +0100

    [INTRO] add CRUD basic calls for API

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,9 @@ app.get('/', async (req: Request, res: Response) => {
     const database = client.db('tech-manager');
     const collection = database.collection('tickets');
 
-    const query = { name: 'starting' };
-    const ticket = await collection.findOne(query);
-    console.log(ticket);
+    const query = { id: req.body.id };
+    const response = await collection.findOne(query);
+    console.log(response);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -25,7 +25,53 @@ app.get('/', async (req: Request, res: Response) => {
 
   res.send('Welcome to Express & TypeScript Server');
 });
-app.post('/');
+
+app.post('/', async (req: Request, res: Response) => {
+  try {
+    const database = client.db('tech-manager');
+    const collection = database.collection('tickets');
+
+    const response = await collection.insertOne(req.body);
+    console.log(response);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.put('/', async (req: Request, res: Response) => {
+  try {
+    const database = client.db('tech-manager');
+    const collection = database.collection('tickets');
+
+    const query = { id: req.body.id };
+    const response = await collection.updateOne(query, req.body);
+    console.log(response);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.delete('/', async (req: Request, res: Response) => {
+  try {
+    const database = client.db('tech-manager');
+    const collection = database.collection('tickets');
+
+    const query = { id: req.body.id };
+    const response = await collection.deleteOne(query);
+    console.log(response);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
+  res.send('Welcome to Express & TypeScript Server');
+});
 
 app.listen(port, () => {
   console.log(`Server is operational on http://localhost:${port}`);
